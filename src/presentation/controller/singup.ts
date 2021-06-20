bac0c574e62f24807de15c1979bb57cdc51740cb
diogojorge1401 <diogojorge1401@gmail.com>
2021-06-20 19:49:53 -0300

    refactor: move all error imports to a single file

--- src/presentation/controller/singup.ts
+++ src/presentation/controller/singup.ts
@@ -1,30 +1,30 @@
 import { HttpResponse, HttpRequest } from "../protocols/http"
-import { MissingParamError } from "../errors/missing-param-error"
+import { MissingParamError, InvalidParamError } from "../errors"
 import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
-import { InvalidParamError } from "../errors/invalid-params-error"
 
-export class SingUpController implements Controller{
-    private readonly emailValidator:EmailValidator
-    constructor(emailValidator:EmailValidator){
+
+export class SingUpController implements Controller {
+    private readonly emailValidator: EmailValidator
+    constructor(emailValidator: EmailValidator) {
         this.emailValidator = emailValidator
     }
     handle(httpRequest: HttpRequest): HttpResponse {
-        try{
-            const requiredFields = ['name', 'email','password',
-            'passwordConfirmation']
+        try {
+            const requiredFields = ['name', 'email', 'password',
+                'passwordConfirmation']
             for (const field of requiredFields) {
                 if (!httpRequest.body[field]) {
                     return badRequest(new MissingParamError(field))
                 }
             }
-            const isValid =this.emailValidator.isValid(httpRequest.body.email)
-            if(!isValid) {
+            const isValid = this.emailValidator.isValid(httpRequest.body.email)
+            if (!isValid) {
                 return badRequest(new InvalidParamError('email'))
-            } 
+            }
         }
-        catch(erro){
+        catch (erro) {
             return serverError()
         }
     }
