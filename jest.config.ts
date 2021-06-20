1337dd57782b92092ae6683d552d2982ee174db9
diogojorge1401 <diogojorge1401@gmail.com>
2021-06-19 21:58:08 -0300

    chore: esqueci  antes

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 export default {
-  roots:['<rootDir/src'],
-  collectCoverageFrom: ['<rootDir/src/**/*.ts>'],
+  roots:['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   testEnvironment: "node",
