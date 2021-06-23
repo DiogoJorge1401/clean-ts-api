1662fa754eb598e18c21c7c5928424f8ad3206aa
diogojorge1401 <diogojorge1401@gmail.com>
2021-06-22 21:50:15 -0300

    refactor: change thinks

--- src/utils/email-validator.ts
+++ src/utils/email-validator.ts
@@ -1,5 +1,5 @@
 import { EmailValidator } from "../presentation/protocols/email-validator"
-export class EmailValidatorAdapter implements EmailValidatorAdapter{
+export class EmailValidatorAdapter implements EmailValidator{
   isValid(email:string):boolean{
     return false
   }
