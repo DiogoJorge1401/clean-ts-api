80bfdf82262cd5fcb98347aab740e6dc1c7f2459
diogojorge1401 <diogojorge1401@gmail.com>
2021-06-25 09:29:33 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/singup-routes.test.ts
+++ src/main/routes/singup-routes.test.ts
@@ -1,7 +1,18 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('SingUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/singup')
@@ -9,7 +20,7 @@ describe('SingUp Routes', () => {
         name: 'Diogo',
         email: 'diogojorge1401@gmail.com',
         password: '1234',
-        passwordConfirmation:'1234'
+        passwordConfirmation: '1234'
       })
       .expect(200)
   })
