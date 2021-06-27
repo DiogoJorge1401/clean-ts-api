951a3bb5c4feb77080c78b360bb540916032f57d
diogojorge1401 <diogojorge1401@gmail.com>
2021-06-27 11:25:41 -0300

    refactor: remove code duplicate

--- src/presentation/controller/login/login.ts
+++ src/presentation/controller/login/login.ts
@@ -4,36 +4,39 @@ import { badRequest, serverError } from "../../helpers/http-helper";
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 import { EmailValidator } from "../singup/singup-protocols";
 
-export class LoginController implements Controller{
+export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator
   private readonly authentication: Authentication
-  
-  constructor(emailValidator:EmailValidator,authentication:Authentication){
-    this.emailValidator= emailValidator
+
+  constructor(emailValidator: EmailValidator, authentication: Authentication) {
+    this.emailValidator = emailValidator
     this.authentication = authentication
   }
-  async handle(httpRequest: HttpRequest): Promise<HttpResponse>{
-    try{
-      const {email,password} = httpRequest.body
-    const isValid = this.emailValidator.isValid(email)
-    
-    if(!email){
-      return new Promise(resolve=>resolve(badRequest(new MissingParamError('email'))))
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+
+      const { email, password } = httpRequest.body
+
+      const isValid = this.emailValidator.isValid(email)
+
+      const requiredFields = ['email', 'password']
+
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+
+      await this.authentication.auth(email, password)
+
     }
-    if(!password){
-      return new Promise(resolve=>resolve(badRequest(new MissingParamError('password'))))
+    catch (error) {
+      return serverError(error)
     }
 
-    if(!isValid){
-      return new Promise(resolve=>resolve(badRequest(new InvalidParamError('email'))))
-    }
-
-    await this.authentication.auth(email,password)
-    
-    }
-    catch(error){
-      return serverError(error )
-    }
-    
   }
 }
