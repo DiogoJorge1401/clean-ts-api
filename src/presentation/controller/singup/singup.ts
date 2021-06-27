55c1b9ca9bbab99cff217fcee1d4522a5590dd71
diogojorge1401 <diogojorge1401@gmail.com>
2021-06-27 16:31:00 -0300

    refactor: move EmailValidation to the ValidationComposite

--- src/presentation/controller/singup/singup.ts
+++ src/presentation/controller/singup/singup.ts
@@ -1,32 +1,21 @@
-import { HttpResponse, HttpRequest, Controller, EmailValidator, AddAccount,Validation } from "./singup-protocols"
-import { InvalidParamError } from "../../errors"
+import { HttpResponse, HttpRequest, Controller, AddAccount, Validation } from "./singup-protocols"
 import { badRequest, ok, serverError } from '../../helpers/http-helper'
 
 export class SingUpController implements Controller {
-    private readonly emailValidator: EmailValidator
     private readonly addAccount: AddAccount
     private readonly validation: Validation
 
-    constructor(emailValidator: EmailValidator, addAccount: AddAccount,validation:Validation) {
-        this.emailValidator = emailValidator
+    constructor(addAccount: AddAccount, validation: Validation) {
         this.addAccount = addAccount
         this.validation = validation
     }
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
             const error = this.validation.validate(httpRequest.body)
-
-            if(error) {
+            if (error) {
                 return badRequest(error)
             }
-            
             const { name, email, password } = httpRequest.body
-
-            const isValid = this.emailValidator.isValid(email)
-            if (!isValid) {
-                return badRequest(new InvalidParamError('email'))
-            }
-
             const account = await this.addAccount.add({
                 name,
                 email,
