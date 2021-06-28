d343fc7cd6e9f97b2ad80f0fc57ff25ee7b7cba4
diogojorge1401 <diogojorge1401@gmail.com>
2021-06-28 13:45:57 -0300

    feat: add LoginValidationFactory

--- src/presentation/controller/singup/singup-protocols.ts
+++ src/presentation/controller/singup/singup-protocols.ts
@@ -1,5 +1,4 @@
 export * from '../../protocols/index'
-export * from '../../protocols/email-validator'
 export * from '../../protocols/http'
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
