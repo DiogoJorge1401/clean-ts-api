1774c39c4c2f0e8713a659c514f27d2ba14e219b
diogojorge1401 <diogojorge1401@gmail.com>
2021-06-29 11:23:35 -0300

    feat: ensure DbAuthentication returns a token on success

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -3,16 +3,16 @@ import { HashCompare } from "../../protocols/criptography/hash-compare";
 import { TokenGenerator } from "../../protocols/criptography/token-generate";
 import { LoadAccountByEmailRepository } from "../../protocols/db/load-account-by-email-repository";
 
-export class DbAuthentication implements Authentication{
-  private readonly loadAccountByEmailRepositoryStub:LoadAccountByEmailRepository
-  private readonly hashCompareStub:HashCompare
-  private readonly tokenGeneratorStub:TokenGenerator
+export class DbAuthentication implements Authentication {
+  private readonly loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
+  private readonly hashCompareStub: HashCompare
+  private readonly tokenGeneratorStub: TokenGenerator
 
   constructor(
-    loadAccountByEmailRepositoryStub:LoadAccountByEmailRepository,
-    hashCompareStub:HashCompare,
-    tokenGeneratorStub:TokenGenerator
-    ){
+    loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository,
+    hashCompareStub: HashCompare,
+    tokenGeneratorStub: TokenGenerator
+  ) {
     this.loadAccountByEmailRepositoryStub = loadAccountByEmailRepositoryStub
 
     this.hashCompareStub = hashCompareStub
@@ -20,15 +20,17 @@ export class DbAuthentication implements Authentication{
     this.tokenGeneratorStub = tokenGeneratorStub
   }
 
-  async auth(authentication:AuthenticationModel): Promise<string>{
+  async auth(authentication: AuthenticationModel): Promise<string> {
     const account = await this.loadAccountByEmailRepositoryStub.load(authentication.email)
 
-    if(account){
-      await this.hashCompareStub.compare(authentication.password,account.password)
+    if (account) {
+      const isValid = await this.hashCompareStub.compare(authentication.password, account.password)
+      if (isValid) {
+        const accessToken = await this.tokenGeneratorStub.generate(account.id)
 
-      await this.tokenGeneratorStub.generate(account.id)
+        return accessToken
+      }
     }
-
     return null
   }
 }
