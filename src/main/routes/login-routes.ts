f2786d4e2a500111d6e1486cd126eb39358be5f7
diogojorge1401 <diogojorge1401@gmail.com>
2021-06-30 12:41:58 -0300

    feat: add login route

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-
-import {makeSingUpController} from '../factories/singup/singup-factory'
-
 import { adaptRoute } from "../adapters/express/express-route-adapter";
 
-export default (router:Router):void => {
-  router.post('/singup',adaptRoute(makeSingUpController()))
+import { makeSingUpController } from '../factories/singup/singup-factory'
+import { makeLoginController } from "../factories/login/login-factory";
+
+export default (router: Router): void => {
+  router.post('/singup', adaptRoute(makeSingUpController()))
+  router.post('/login', adaptRoute(makeLoginController()))
 }
