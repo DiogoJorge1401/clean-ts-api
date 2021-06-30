63ffa0da56b13abdc4087a53abccf842be62fb0f
diogojorge1401 <diogojorge1401@gmail.com>
2021-06-30 12:00:41 -0300

    refactor: rename singup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
-describe('SingUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -13,15 +13,17 @@ describe('SingUp Routes', () => {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/singup')
-      .send({
-        name: 'Diogo',
-        email: 'diogojorge1401@gmail.com',
-        password: '1234',
-        passwordConfirmation: '1234'
-      })
-      .expect(200)
+  describe('POST /singup', () => {
+    test('Should return 200 on sinup', async () => {
+      await request(app)
+        .post('/api/singup')
+        .send({
+          name: 'Diogo',
+          email: 'diogojorge1401@gmail.com',
+          password: '1234',
+          passwordConfirmation: '1234'
+        })
+        .expect(200)
+    })
   })
 })
--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
+
 import {makeSingUpController} from '../factories/singup/singup-factory'
+
 import { adaptRoute } from "../adapters/express/express-route-adapter";
+
 export default (router:Router):void => {
   router.post('/singup',adaptRoute(makeSingUpController()))
 }
