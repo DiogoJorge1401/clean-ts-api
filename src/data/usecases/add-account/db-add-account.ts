6d9832067b57e10ccd838aa8e5cba6581392a2ba
diogojorge1401 <diogojorge1401@gmail.com>
2021-07-02 14:33:25 -0300

    feat: ensure DbAddAccount returns null if LoadAccountByEmailRepository returns an account

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,26 +1,30 @@
-import { 
+import {
   AccountModel,
-  Hasher,AddAccount, 
-  AddAccountModel, 
-  AddAccountRepository, 
-  LoadAccountByEmailRepository 
+  Hasher, AddAccount,
+  AddAccountModel,
+  AddAccountRepository,
+  LoadAccountByEmailRepository
 } from './db-add-account-protocols';
 
-export class DbAddAccount implements AddAccount{
+export class DbAddAccount implements AddAccount {
 
   constructor(
     protected hasher: Hasher,
-    protected addAccountRepository:AddAccountRepository,
-    protected loadAccountByEmailRepository:LoadAccountByEmailRepository
-    ){}
-  
-  async add(accountData:AddAccountModel): Promise<AccountModel>{
-    await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
+    protected addAccountRepository: AddAccountRepository,
+    protected loadAccountByEmailRepository: LoadAccountByEmailRepository
+  ) { }
 
-    const hashedPassword = await this.hasher.hash(accountData.password)
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const account = await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
 
-    const account=await this.addAccountRepository.add(Object.assign({},accountData,{password:hashedPassword}))
+    if (!account) {
+      const hashedPassword = await this.hasher.hash(accountData.password)
 
-    return account
+      const newAccount = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+
+      return newAccount
+    }
+    return null
+
   }
 }
